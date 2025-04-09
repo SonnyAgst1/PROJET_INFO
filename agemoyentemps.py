5b58cd240386c7f3f2113b3c914921dc49ee7d5b
AUGUSTO Sonny <id2717@DOMENSAI.ECOLE>
2025-04-09 15:59:18 +0200

    évolution de l'age moyen des athlètes

---
+++ agemoyentemps.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# Charger la base de données
+df = pd.read_csv("donnees_jeux_olympiques/athlete_events.csv")
+
+age_moyen_par_annee = df.groupby("Year")["Age"].mean()
+
+plt.figure(figsize=(10, 5))
+plt.plot(age_moyen_par_annee.index, age_moyen_par_annee.values, marker="o")
+plt.title("Age moyen des athlètes par années")
+plt.xlabel("Année")
+plt.ylabel("Age moyen des athlètes")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
