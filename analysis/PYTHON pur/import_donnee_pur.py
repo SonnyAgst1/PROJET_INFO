a79903b2cffbdd6d7ca762000fcfde03957d38ff
AUGUSTO Sonny <id2717@DOMENSAI.ECOLE>
2025-04-30 09:50:48 +0200

    import_donnee_pur

--- analysis/PYTHON pur/import_donnee_pur.py
+++ analysis/PYTHON pur/import_donnee_pur.py
@@ -1,1 +1,8 @@
+def importdonneespur(chemin):
+    data = []
 
+    with open(chemin, newline='', encoding="utf-8") as f:
+        lecteur = csv.DictReader(f)
+        for ligne in lecteur:
+            data.append(ligne)
+    return data
