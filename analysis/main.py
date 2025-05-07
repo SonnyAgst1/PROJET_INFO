3c4c294b1807c1fbb5365d3eacb2ffc126b3f6ca
id2755-ensai <roua.ben-youssef@eleve.ensai.fr>
2025-05-07 16:21:04 +0200

    Update main.py
    version sans q10 et q5

--- analysis/main.py
+++ analysis/main.py
@@ -1,189 +1,304 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-from import_donnee_panda import importdonneepanda
-import csv
-from collections import defaultdict
-from import_donnee_pur import importdonneespur
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-
+from PANDAS.importation_donnees_panda import importdonneepanda
+from PYTHON_pur.importation_donnees_pypur import importdonneespur
+import time
 import os
+from PANDAS.plot_correlation_athletes_vs_medals import (
+    plot_correlation_athletes_vs_medals,
+)
+from PYTHON_pur.nb_médailles_athlete import nbr_medailles
+from PANDAS.nb_medailles_athlete_pd import nbr_medailles_pd
+
+from PANDAS.corr_taille_poids_performances import (
+    corr_taille_poids_performances
+)
+from PANDAS.Profil_moyen import profil_moyen
+from PANDAS.performance_host import analyser_gain_pays_hote
+from PYTHON_pur.Nbr_medaille_pays import moyenne_medailles_pur
+from PANDAS.nbr_med_pays_co_vs_ind import graphique_medailles_par_type
+from PYTHON_pur.agemoyentemps_pur import agemoyenathlètes
+from PANDAS.prediction import (
+    predict_medal_chance)
+
+# creation du fichier rapport
 os.makedirs("resultats", exist_ok=True)
 
-df_panda = importdonneepanda("analysis/donnees_jeux_olympiques/athlete_events.csv")
-df_pur = importdonneespur("analysis/donnees_jeux_olympiques/athlete_events.csv")
 
-#---------------------------------------------------------------------------------------------
+# importation des données
+
+df_panda = importdonneepanda(
+    "PROJET_INFO/analysis/donnees_jeux_olympiques/athlete_events.csv"
+)
+df_pur = importdonneespur(
+    "PROJET_INFO/analysis/donnees_jeux_olympiques/athlete_events.csv"
+)
+
+
+# ---------------------------------------------------------------------------------------------
 # Question 1 : Déterminer le nombre de médaille gagné par Micheal Phelps
 
-#Resultat Avec Python pur:
+# Resultat Avec Python pur:
+start_time = time.time()
 
-from Nbr_medaille_athlète import nbr_medailles
 
 with open("resultats/rapport.txt", "a", encoding="utf-8") as f:
-    f.write(f"Question 1 : Déterminer le nombre de médaille gagné par un athlète (l'exemple de Micheal Phelps):\n")
-    f.write(f"- Version 1 = Avec Pyhton pur:\n")
-    
+    f.write(
+        "Question 1 : Déterminer le nombre de médaille gagné par un athlète"
+        ":l'exemple de Micheal Phelps:\n \n"
+    )
+    f.write("- Version 1 = Avec Pyhton pur:\n")
+
+
+# Calcul du tps d'exectution
+
 athlete = "Michael Fred Phelps, II"
-medailles = nbr_medailles_pur(df_pur, athlete)
+medailles = nbr_medailles(df_pur, athlete)
+
+
+total_medals = nbr_medailles(df_pur, athlete)
+
+gold = total_medals.get("Gold", 0)
+silver = total_medals.get("Silver", 0)
+bronze = total_medals.get("Bronze", 0)
+total = gold + silver + bronze
+
 
 # Affichage dans le terminal
 print(f"Nombre de médailles de {athlete} :")
 for type_medaille, nb in medailles.items():
     print(f"  {type_medaille} : {nb}")
+print("Nombre total de médailles gagnées :", total)
+
+end_time = time.time()
+execution_time = end_time - start_time
+
+# tps d'execution du code python pur
+print(f" Temps d'exécution : {execution_time:.2f} secondes")
 
 # Rajout au rapport
 with open("resultats/rapport.txt", "a", encoding="utf-8") as f:
-    f.write(f"Nombre de médailles de {athlete} (Python pur) :\n")
+    f.write(f" Nombre de médailles de {athlete} (Python pur) :\n")
     for type_medaille, nb in medailles.items():
         f.write(f"  {type_medaille} : {nb}\n")
-    f.write("\n")
+    f.write(f" Nombre total de médailles gagnées : {total} \n")
+    f.write(" Temps d'exécution (python pur)"
+            f": {execution_time:.2f} secondes \n \n")
 
-#---------------------------------------------------------------------------------------------------
 
-# Question 2 : Quels sont les sports où la taille et le poids influencent le plus les performances ?
+# --------------
+# Resultat Avec Pandas:
+start_time = time.time()
 
-from sportmedal_correlation import sport_medal_correlation
 
-correlations = sport_medal_correlation(df_panda)
-print("Quels sont les sports où la taille et le poids influencent le plus les performances ?")
+athlete_name = "Michael Fred Phelps, II"
+total_medals = nbr_medailles_pd(df_panda, athlete_name)
+
+gold = total_medals.get("Gold", 0)
+silver = total_medals.get("Silver", 0)
+bronze = total_medals.get("Bronze", 0)
+total = gold + silver + bronze
+
+print("OR :", gold, "ARGENT :", silver, "BRONZE :", bronze)
+print("Nombre total de médailles gagnées :", total)
+
+
+with open("resultats/rapport.txt", "a", encoding="utf-8") as f:
+    f.write("- Version 2 = Avec Pandas:\n")
+
+# Affichage dans le terminal
+print(f"Nombre de médailles de {athlete} :")
+for type_medaille, nb in medailles.items():
+    print(f"  {type_medaille} : {nb}")
+print("Nombre total de médailles gagnées :", total)
+
+end_time = time.time()
+execution_time = end_time - start_time
+
+# tps d'execution du code python pur
+print(f" Temps d'exécution : {execution_time:.2f} secondes \n \n")
+
+# Rajout au rapport
+with open("resultats/rapport.txt", "a", encoding="utf-8") as f:
+    f.write(f" Nombre de médailles de {athlete} (Python pur) :\n")
+    for type_medaille, nb in medailles.items():
+        f.write(f"  {type_medaille} : {nb}\n")
+    f.write(f" Nombre total de médailles gagnées : {total} \n")
+    f.write(" Temps d'exécution (pandas) :"
+            f"{execution_time:.2f} secondes \n \n")
+
+
+# ---------------------------------------------------------------------------------------------------
+
+# Question 2 : Quels sont les sports où la taille et le poids influencent
+# le plus les performances ?
+
+
+results = corr_taille_poids_performances(df_panda)
+
+correlations = pd.DataFrame(results).sort_values(
+    by="Combined", ascending=False)
+print("Sports où la taille et le poids sont les plus liés"
+      "au nombre de médailles :")
+
 print(correlations.head(10))
 
-# Ajoute une colonne d'identification de l'analyse
-correlations["Analyse"] = "Corrélation taille/poids vs médailles"
-
-# Réorganiser les colonnes (optionnel)
-cols = ["Analyse", "Sport", "Corr_Height", "Corr_Weight", "Combined"]
-correlations = correlations[cols]
-
-# Ajouter au fichier CSV global
-correlations.to_csv("resultats/tableaux.csv", mode="a", index=False)
+with open("resultats/rapport.txt", "a", encoding="utf-8") as f:
+    f.write(
+        "Question2: Quels sont les sports où la taille et le poids influencent"
+        "le plus les performances:\n"
+    )
+    f.write(f"{correlations.head(10)}\n \n")
 
 
-
-
-#----------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------
 
 # Question 3 : Quel est le profil type d'un médaillé selon la discipline ?
 
-from Profil_moyen import profil_moyen
 
-print("Quel est le profil type d'un médaillé selon la discipline ?")
-result = profil_moyen(df_panda)
-print(result.head())
-
-# Sauvegarde du résultat
-result.reset_index().assign(Analyse="Profil moyen par sport").to_csv("resultats/tableaux.csv", mode="a", index=False
-
-
-
-#-------------------------------------------------------------------------------------------------------
-
-# Question 4 : Y a t-il une relation entre la taille d'un pays (nombre d'athlète) et son nombre de médaille
-
-from plot_correlation_athletes_vs_medals import plot_correlation_athletes_vs_medals
-
-print(" Y a t-il une relation entre la taille d'un pays (nombre d'athlète) et son nombre de médaille ?")
-plt_obj = plot_correlation_athletes_vs_medals(df_panda)
-plt_obj.savefig("resultats/correlation_athletes_vs_medailles.png")
-plt_obj.close()
-
-
-#------------------------------------------------------------------------------------------------------
-
-# Question 5 : Trouver les bornes inférieur et supérieur du nombre de médaille par nation en 2016
-
-
-from borne_inf_sup import analyser_jo_par_annee
-chemin = "donnees_jeux_olympiques/donnees_jeux_olympiques/athlete_events.csv"
-print("Trouver les bornes inférieur et supérieur du nombre de médaille par nation en 2016 :")
-analyser_jo_par_annee( chemin , 2016 )
-annee = 2016
-from io import StringIO
-import sys
-
-buffer = StringIO()
-sys.stdout = buffer
-
-analyser_jo_par_annee(chemin, annee)
-
-sys.stdout = sys.__stdout__
+profil_type_top20_sexe = profil_moyen(df_panda)
+# Affichage
+print("Profil moyen des 20 athlètes les plus médaillés"
+      "par sport et par sexe :")
+print(profil_type_top20_sexe)
 with open("resultats/rapport.txt", "a", encoding="utf-8") as f:
-    f.write(f"Résultats pour les JO de {annee} :\n")
-    f.write(buffer.getvalue())
-    f.write("\n")
+    f.write(
+        "Question 3 : Quel est le profil type d'un médaillé"
+        " selon la discipline par sexe ?: \n"
+    )
+    f.write(f"{profil_type_top20_sexe.head(20)}\n \n")
 
 
-#----------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------
+
+# Question 4 : Y a t-il une relation entre la taille d'un pays
+#  (nombre d'athlète) et son nombre de médaille
+
+
+print(
+    " Y a t-il une relation entre la taille d'un pays (nombre d'athlète)"
+    "et son nombre de médaille ?"
+)
+plot_correlation_athletes_vs_medals(df_panda)
+
+with open("resultats/rapport.txt", "a", encoding="utf-8") as f:
+    f.write(
+        " Question 4 : Y a t-il une relation entre la taille d'un pays"
+        "nombre d'athlète et son nombre de médaille \n"
+    )
+    f.write(
+        " Le résultat est une courbe 'fichier png' présente dans "
+        "le dossier 'resultats '\n \n"
+    )
+
+
+# ------------------------------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------------------------------
 
 # Question 6 : Est ce que les pays hotes performent mieux que les autres ?
 
-from host_boost import comparaison_host_vs_nonhost
+# Chargement des données
 
-print("Les pays hôtes gagnent-ils plus de médailles lorsqu’ils organisent les JO ?")
-result = comparaison_host_vs_nonhost(df_panda)
-print(result)
+resultats = analyser_gain_pays_hote(df_panda)
 
-# Sauvegarde dans rapport.txt
+# Affichage
+print("Analyse de l'impact du pays hôte sur les performances :\n")
+print(resultats)
+
+# Sauvegarde dans un rapport
 with open("resultats/rapport.txt", "a", encoding="utf-8") as f:
-    f.write("Question : Les pays organisateurs gagnent-ils plus de médailles chez eux ?\n")
-    f.write(result.to_string(index=False))
+    f.write(
+        "Question 6 : Influence du pays hôte sur le nombre"
+        "de médailles gagnées\n"
+    )
+    f.write(resultats.to_string(index=False))
     f.write("\n\n")
 
-# Sauvegarde dans tableaux.csv
-result["Analyse"] = "Médailles pays hôte vs non-hôte"
-result.to_csv("resultats/tableaux.csv", mode="a", index=False)
+
+# Question 7 : Moyenne du nombre de medaille gagné par pays sur
+# toutes les éditions
+
+# Resultat Avec Python pur:
+start_time = time.time()
 
 
+with open("resultats/rapport.txt", "a", encoding="utf-8") as f:
+    f.write(
+        "Question 7 : Moyenne du nombre de medaille gagné par type de"
+        "sport sur toutes les éditions:\n \n"
+    )
+    f.write("- Version 1 = Avec Pyhton pur:\n")
 
+resultats = moyenne_medailles_pur(df_pur)
 
-# Question 7 : Moyenne du nombre de medaille gagné par pays sur toutes les éditions
-
-from Nbr_medaille_pays import moyenne_medailles_par_pays_par_annee
-resultats = moyenne_medailles_par_pays_par_annee(df_pur)
-
-# Afficher les 10 premiers
-print("Top 10 des pays ayant remporté le plus de médailles en moyenne par an :")
-for team, moyenne in resultats[:10]:
-    print(f"{team} : {moyenne:.2f} médailles/an")
 
 # Sauvegarder dans rapport.txt
 with open("resultats/rapport.txt", "a", encoding="utf-8") as f:
-    f.write("Question : Quelle est la moyenne de médailles par pays sur toutes les éditions ?\n")
-    for team, moyenne in resultats[:10]:
-        f.write(f"{team} : {moyenne:.2f} médailles/an\n")
-    f.write("\n")
+    f.write(
+        "Moyenne du nombre de medaille gagné par type de"
+        "sport sur toutes les éditions: \n"
+    )
+    f.write(
+        " Nombre moyen de médailles individuelles par pays :"
+        f"{resultats[0]:.2f} \n"
+        " Nombre moyen de médailles "
+        f"collectives par pays : {resultats[1]:.2f} \n"
+        f" Nombre moyen total de médailles par pays : {resultats[2]:.2f}\n \n"
+    )
 
 
-# Question 8 : Quel est le nombre de médailles remportées par chaque pays, en distinguant celles gagnées dans des sports collectifs de celles gagnées dans des sports individuels ?
+# Question 8 : Quel est le nombre de médailles remportées par chaque pays, en
+#  distinguant celles gagnées dans des sports collectifs de celles gagnées dans
+#  des sports individuels ?
 
-from nbr_med_pays_co_vs_ind import graphique_medailles_par_type
-
-print("Quel est le nombre de médailles remportées par chaque pays, en distinguant celles gagnées dans des sports collectifs de celles gagnées dans des sports individuels ?")
+print(
+    "Quel est le nombre de médailles remportées par chaque pays, en"
+    "distinguant celles gagnées dans des sports collectifs de celles gagnées"
+    " dans des sports individuels ?"
+)
 graphique_medailles_par_type(df_panda, top_n=10)
-plt_obje = graphique_medailles_par_type(df_panda, top_n=10)
-plt_obje.savefig("resultats/medailles_collectif_vs_individuel.png")
 
 
+with open("resultats/rapport.txt", "a", encoding="utf-8") as f:
+    f.write(
+        " Question 8 : Quel est le nombre de médailles remportées par "
+        "chaque pays, en"
+        "distinguant celles gagnées dans des sports collectifs de celles \n"
+        " gagnées dans"
+        "des sports individuels ? \n \n"
+    )
+    f.write(
+        " Le résultat est un barchart 'fichier png' présent dans "
+        "le dossier 'resultats ' \n \n"
+    )
+
+# Question 9 : Evolution de l'age moyen des athlètes au cours
+# du temps ( graphique )
 
 
+print(
+    "Question 9:Comment a évolué l'age moyen des athlètes au cours du temps ?",
+    agemoyenathlètes(df_pur)
+)
 
-# Question 9 : Evolution de l'age moyen des athlètes au cours du temps ( graphique )
-
-from agemoyentemps_pur import agemoyenathlètes
-
-print("Comment a évolué l'age moyen des athlètes au cours du temps ? ", agemoyenathlètes(df_pur))
-
+with open("resultats/rapport.txt", "a", encoding="utf-8") as f:
+    f.write("Question 9 : Comment a évolué l'âge moyen des athlètes "
+            "au cours du temps ? \n \n")
+    f.write(agemoyenathlètes(df_pur))
+    f.write("\n\n")
 
 # Question 10 :
 
+# ----------------------------------------------------------------------------
+# -------------------------
+# Question de prédiction :
+# ------------------------
+
+#  Est-il possible de prédire si un athlète gagnera
+#  une médaille en fonction de caractéristique ?
 
 
-# Question de prédiction : Est-il possible de prédire si un athlète gagnera une médaille en fonction de caractéristique ?
-
-from prediction import (train_and_save_model, predict_medal_chance )
-
-# Prédiction
 age = 25
 height = 195
 weight = 95
@@ -191,13 +306,27 @@ sex = "M"
 sport = "Basketball"
 noc = "USA"
 
+
+# Prédiction directe (le modèle s'entraîne une seule fois puis est réutilisé)
 proba = predict_medal_chance(df_panda, age, height, weight, sex, sport, noc)
 
-# Affichage à l'écran
-print(f"✅ Probabilité qu’un athlète de {noc} en {sport} remporte une médaille : {proba:.2%}")
+print(f"Probabilité de gagner une médaille : {proba:.2%}")
+
+print(
+    f" Probabilité qu’un athlète de {noc} en {sport} "
+    f"remporte une médaille : {proba:.2%}"
+)
 
 # Sauvegarde dans le rapport
 with open("resultats/rapport.txt", "a", encoding="utf-8") as f:
-    f.write("Question : Peut-on prédire si un athlète va remporter une médaille ?\n")
-    f.write(f"→ Pour un athlète {sex}, {age} ans, {height} cm, {weight} kg, "
-            f"sport : {sport}, pays : {noc} → Probabilité de médaille : {proba:.2%}\n\n")
+    f.write(" PROBLEMATIQUE :\n")
+    f.write("-Question de prédiction : Peut-on prédire "
+            "si un athlète va remporter"
+            " une médaille en fonction de ses caractéristiques "
+            "(age, poids, taille, sexe, nationalité, "
+            "le sport qu'il pratique )?\n")
+    f.write(
+        f"→ Pour un athlète {sex}, {age} ans, {height} cm, {weight} kg, "
+        f"sport : {sport}, pays : {noc} → Probabilité de médaille :"
+        f"{proba:.2%}\n\n"
+    )
