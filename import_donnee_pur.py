102581f861caa4358fb08a040a2a9a7c40949de9
id2755-ensai <roua.ben-youssef@eleve.ensai.fr>
2025-05-07 20:25:46 +0200

    Delete import_donnee_pur.py

--- import_donnee_pur.py
+++
@@ -1,8 +0,0 @@
-def importdonneespur(chemin):
-    data = []
-
-    with open(chemin, newline='', encoding="utf-8") as f:
-        lecteur = csv.DictReader(f)
-        for ligne in lecteur:
-            data.append(ligne)
-    return data
